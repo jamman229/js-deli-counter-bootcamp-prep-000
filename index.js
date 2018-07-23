3b5472871d4998f84ebc7895d450e2947cf94cdc
Alex Caswell <caswellyatsenko@gmail.com>
2018-07-23 02:39:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(katzDeli){
     return "The line is currently empty."
   }else{
     for (var i = 0; i < katzDeli.length; i++){
-      return `The line is currently: ${i}. ${katzDeli[i]}`
+      return `The line is currently: ${i+1}. ${katzDeli[i]}`
     }
   }
 }
