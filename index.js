8e82c165f5563c4efaa327d0691a6c28e37d9ce7
Alex Caswell <caswellyatsenko@gmail.com>
2018-07-23 02:56:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,19 +13,17 @@ function nowServing(katzDeli){
   }
 }
 
-var line = [];
-
-
-
-
-function currentLine(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
-    line.push(` `+[i+1]+`. `  + katzDeli[i])
-    i++;
+function currentLine(katzDeli){
+  if (katzDeli.length === 0){
+    return "The line is currently empty."
+  }else{
+    let i = 0
+    var line = []
+    while (i < katzDeli.length){
+      line.push(` ` +[i+1]+`.` + katzDeli[i])
+      i++;
+        return (`The line is currently:` + line)
+    }
   }
-  if (katzDeli.length === 0) {
-    return "The line is currently empty.";
-  } else
-return(`The line is currently:` + line);
 }
+
