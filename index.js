c0a20e23891cd45a22c96afe3f0880ad681b7ae2
Alex Caswell <caswellyatsenko@gmail.com>
2018-07-23 02:05:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var katzDeli = []
 
 function takeANumber(katzDeli, name){
   katzDeli.push(name)
-  return `Welcome, Ada. You are number ${katzDeli.length} in line.`
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
 }
 
